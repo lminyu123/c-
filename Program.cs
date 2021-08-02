15d5149e5957b8bddb348dd97f316dc720b27bc3
lminyu123 <61035433+lminyu123@users.noreply.github.com>
2021-08-02 01:15:06 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,61 +1,35 @@
 ﻿using System;
 
-namespace Exercise7
+namespace Exercise5
 {
     class Program
     {
         static void Main(string[] args)
-
-
         {
-            int pin, choice;
-            int deposit, withdraw, updatemoney, money = 1000;
-            Console.WriteLine("Enter you Pin Number");
-            pin = Convert.ToInt32(Console.ReadLine());
+            int rnum,lastint=0, cnum,i;
+            Console.WriteLine("Enter the number of rows: ");
+            rnum = int.Parse(Console.ReadLine());
 
-
-            Console.WriteLine("********Welcome to ATM Service**************");
-            Console.WriteLine("1. Check Balance");
-            Console.WriteLine("2. Withdraw Cash");
-            Console.WriteLine("3. Deposit Cash");
-            Console.WriteLine("4. Quit");
-
-
-            Console.WriteLine("*********************************************");
-            Console.WriteLine("Enter your Choice:");
-            choice = int.Parse(Console.ReadLine());
-            switch (choice)
+            Console.WriteLine("Result: ");
+            for (i = 1; i <= rnum; i++)
             {
-                case 1:
-                    Console.WriteLine("\n YOUR BALANCE IN Rs : {0} ", money);
-                    break;
-                case 2:
-                    Console.WriteLine("\n Enter the withdraw money:");
-                    withdraw = int.Parse(Console.ReadLine());
-                    if (withdraw > (money))
+                for (cnum=1; cnum<=i; cnum++)
+                {
+                    if (lastint == 1)
                     {
-                        Console.WriteLine("INSUFFICENT BALANCE");
+                        Console.Write("0");
+                        lastint = 0;
                     }
-                    else
-                        {
-                        updatemoney = money - withdraw;
-                        Console.WriteLine("\n\n PLEASE COLLECT CASH");
-                        Console.WriteLine("\n YOUR CURRENT BALANCE IS {0}", updatemoney);
-                        }
-
-                    break;
-                case 3:
-                    Console.WriteLine("\n ENTER THE AMOUNT TO DEPOSIT");
-                    deposit = int.Parse(Console.ReadLine());
-                    updatemoney = money + deposit;
-                    Console.WriteLine("YOUR BALANCE IS {0}", updatemoney);
-                    break;
-                case 4:
-                    Console.WriteLine("\n THANK U USING ATM");
-                    break;
+                    else if (lastint == 0)
+                    {
+                        Console.Write("1");
+                        lastint = 1;
+                    }
+                }
+                Console.Write("\n");
             }
+            Console.ReadLine();
+
         }
-        
     }
 }
-    
