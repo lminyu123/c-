1c1bd6e3b49391ba003c185424093ae41c1023de
lminyu123 <61035433+lminyu123@users.noreply.github.com>
2021-08-02 01:20:37 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your start number : ");
+            int StartNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your end number : ");
+            int EndNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The Armstrong Numbers between {StartNumber} and {EndNumber} are : ");
+            for (int i=StartNumber; i<=EndNumber; i++)
+            {
+                if (ArmstrongNumber(i))
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+
+        private static bool ArmstrongNumber(int number)
+        {
+            int sum = 0;
+            int tempnumber = number;
+            int temp = 0;
+            int length = number.ToString().Length;
+
+            while(number !=0)
+            {
+                temp = number % 10;
+                number = number / 10;
+                sum += (int)Math.Pow(temp, length);
+                //sum =sum+(temp*temp*temp);
+            }
+            if (sum ==tempnumber)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
