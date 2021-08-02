3f52e5c72e67c7ec18070928dffb18972c066d90
lminyu123 <61035433+lminyu123@users.noreply.github.com>
2021-08-02 01:21:19 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "", reverse = "";
+            int Length = 0;
+            Console.WriteLine("Enter your Word");
+            str = Console.ReadLine();
+            Length = str.Length - 1;
+            while (Length>=0)
+            {
+                reverse = reverse + str[Length];
+                Length--;
+            }
+
+            //display the reverse word
+            Console.WriteLine("Reverse word is: \n {0}", reverse);
+            Console.ReadLine();
+            
+        }
+
+
+    }
+}
