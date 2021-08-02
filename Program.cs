b26bb205ac983daa0bda45d2604ccf9d81e3a5f8
lminyu123 <61035433+lminyu123@users.noreply.github.com>
2021-08-02 01:19:04 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rnum, emptnum, i;
+            Console.WriteLine("Ener the number of row");
+            rnum = int.Parse(Console.ReadLine());
+
+            //start row  i=1, i=2, i=3
+            for(i=1; i<=rnum; i++)
+            {
+                // count empty space 
+                for(emptnum=1;emptnum<=rnum-i; emptnum++)
+                    Console.Write(" ");
+                // count start space
+                for(emptnum=1;emptnum<=i*2-1;emptnum++)
+                    Console.Write("*");
+                    Console.WriteLine();
+
+            }
+            //back row i=2,i=1,i=0
+            for (i = rnum - 1; i>=1; i--)
+            {
+                // 2,1
+                for(emptnum=1;emptnum<=rnum-i; emptnum++)
+                    Console.Write(" ");
+                for(emptnum=1;emptnum<=i*2-1;emptnum++)
+                    Console.Write("*");
+                 Console.WriteLine();
+
+            }
+        }
+    }
+}
